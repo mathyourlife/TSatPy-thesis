aebb067dc8ff84ae897ae511f140d79c69a92e78
dcouture <dcouture@dyn.com>
2013-12-16 23:17:44 -0500

    Allow for transpose vector for quaternion

--- TSatPy/Quaternion.py
+++ TSatPy/Quaternion.py
@@ -10,13 +10,18 @@ class Quaternion(object):
     Attitude quaternion class
     """
 
+    float_threshold = 1e-15
+
     def __init__(self, vector, scalar=None, radians=None):
 
         if scalar is None:
             self.from_rotation(vector, radians)
         else:
-            self.vector = np.mat(vector, dtype=np.float)
+            self.vector = np.mat(vector, dtype=np.float).T
             self.scalar = float(scalar)
+
+        if self.vector.shape == (1, 3):
+            self.vector = self.vector.T
 
     def __eq__(self, q):
 
@@ -40,7 +45,7 @@ class Quaternion(object):
         :return: Magnitude of the quaternion
         :rtype:  float
         """
-        return np.sqrt(np.sum(self.vector * self.vector.T) + self.scalar**2)
+        return np.sqrt(np.sum(self.vector.T * self.vector) + self.scalar**2)
 
     def normalize(self):
         """
@@ -51,16 +56,47 @@ class Quaternion(object):
         self.vector /= mag
         self.scalar /= mag
 
+    def is_unit(self):
+
+        return np.abs(self.mag() - 1) < self.float_threshold
+
     def conj(self):
         """
         Create a conjugate quaternion with the same scalar and the negative vector.
         """
         return Quaternion(-self.vector, self.scalar)
 
+    @property
+    def x(self):
+        """
+        Skew-symetric cross product matrix
+        """
+        return np.mat([
+            [0, -self.vector[2, 0], self.vector[1, 0]],
+            [self.vector[2, 0], 0, -self.vector[0, 0]],
+            [-self.vector[1, 0], self.vector[0, 0], 0],
+        ])
+
+    @property
+    def rmatrix(self):
+
+        s = self.scalar
+        v = self.vector
+        x1 = (s**2 - (v.T * v)[0,0]) * np.eye(3)
+        x2 = 2 * self.vector * self.vector.T
+        x3 = 2 * s * self.x
+
+        return x1 + x2 - x3
+
+    def rotate_points(self, pts):
+
+        r_pts = self.rmatrix * pts.T
+        return r_pts.T
+
     def from_rotation(self, vector, radians):
 
         v = np.matrix(vector, dtype=np.float)
-        v = v/(v*v.T)[0,0]
+        v = v/(v*v.T)[0, 0]
 
         self.vector = v * np.sin(-radians/2)
         self.scalar = np.cos(-radians/2)
@@ -78,9 +114,10 @@ class Quaternion(object):
         return Quaternion(self.vector - q.vector, self.scalar - q.scalar)
 
     def __mul__(self, q):
-        s = self.scalar * q.scalar - (self.vector * q.vector.T)[0,0]
-        v = self.vector * q.scalar + q.vector * self.scalar + np.cross(self.vector, q.vector)
-        return Quaternion(v, s)
+        #here
+        s = self.scalar * q.scalar - (self.vector.T * q.vector)[0, 0]
+        v = self.vector * q.scalar + q.vector * self.scalar + np.cross(self.vector.T, q.vector.T).T
+        return Quaternion(v.T, s)
 
     def __str__(self):
         """
@@ -90,7 +127,15 @@ class Quaternion(object):
 
         return "<%s <%g %g %g>, %g>" % (
             self.__class__.__name__,
-            self.vector[0,0], self.vector[0,1], self.vector[0,2],
+            self.vector[0, 0], self.vector[1, 0], self.vector[2, 0],
             self.scalar)
 
     __repr__ = __str__
+
+
+class Identity(Quaternion):
+
+    def __init__(self):
+        self.vector = np.mat([0, 0, 0])
+        self.scalar = 1
+
--- TSatPy/tests/Quatertion_test.py
+++ TSatPy/tests/Quatertion_test.py
@@ -1,13 +1,14 @@
 import unittest
-from TSatPy.Quaternion import Quaternion
+from TSatPy.Quaternion import Quaternion, Identity
 import numpy as np
 
 
 class TestQuaternionBasics(unittest.TestCase):
 
     def test_init(self):
-        vec = [1, 2, 3]
-        q = Quaternion(vec,  4)
+        vec = np.mat([1, 2, 3]).T
+        q = Quaternion([1, 2, 3],  4)
+
         self.assertTrue(np.all(vec == q.vector))
         self.assertEquals(4, q.scalar)
 
@@ -22,7 +23,7 @@ class TestQuaternionBasics(unittest.TestCase):
         q.normalize()
 
         m = np.sqrt(30)
-        t_vec = [1/m, 2/m, 3/m]
+        t_vec = np.mat([1/m, 2/m, 3/m]).T
         self.assertTrue(np.all(t_vec == q.vector))
         self.assertEquals(4/m, q.scalar)
 
@@ -32,7 +33,7 @@ class TestQuaternionBasics(unittest.TestCase):
         q = Quaternion(vec,  4)
 
         r = q.conj()
-        self.assertTrue(np.all([-1, -2, -3] == r.vector))
+        self.assertTrue(np.all(np.matrix([-1, -2, -3]).T == r.vector))
         self.assertEquals(4, r.scalar)
 
     def test_str(self):
@@ -45,10 +46,10 @@ class TestQuaternionBasics(unittest.TestCase):
 
     def test_definition(self):
 
-        i = Quaternion([1,0,0],0)
-        j = Quaternion([0,1,0],0)
-        k = Quaternion([0,0,1],0)
-        q_neg = Quaternion([0,0,0],-1)
+        i = Quaternion([1, 0, 0], 0)
+        j = Quaternion([0, 1, 0], 0)
+        k = Quaternion([0, 0, 1], 0)
+        q_neg = Quaternion([0, 0, 0], -1)
 
         self.assertEquals(i*i, q_neg)
         self.assertEquals(j*j, q_neg)
@@ -58,24 +59,36 @@ class TestQuaternionBasics(unittest.TestCase):
         self.assertEquals(j*k, i)
         self.assertEquals(k*i, j)
 
+
 def within_threshold(a, b):
-    return (a-b).mag() < 1e-15
+    return (a-b).mag() < Quaternion.float_threshold
+
 
 class TestQuaternionOperations(unittest.TestCase):
 
     def test_eq(self):
 
-        a = Quaternion([1,2,-3], 0.4)
-        b = Quaternion([1,2,-3], 0.4)
-        self.assertEquals(a,b)
+        a = Quaternion([1, 2, -3], 0.4)
+        b = Quaternion([1, 2, -3], 0.4)
+        self.assertEquals(a, b)
 
-        a = Quaternion([1,2,-3], 0.4)
-        b = Quaternion([-1,-2,3], -0.4)
-        self.assertEquals(a,b)
+        a = Quaternion([1, 2, -3], 0.4)
+        b = Quaternion([-1, -2, 3], -0.4)
+        self.assertEquals(a, b)
+
+        a = Quaternion([1, 2, -3], -0.4)
+        b = Quaternion([-1, -2, 3], -0.4)
+        self.assertFalse(a == b)
+
+    def test_neq(self):
+
+        a = Quaternion([1, 2, -3], -0.4)
+        b = Quaternion([-1, -2, 3], -0.4)
+        self.assertNotEquals(a, b)
 
     def test_add(self):
-        a = Quaternion([1,0.2,-3], 0.3)
-        b = Quaternion([2,0.1,-2], 0.8)
+        a = Quaternion([1, 0.2, -3], 0.3)
+        b = Quaternion([2, 0.1, -2], 0.8)
 
         self.assertTrue(
             within_threshold(
@@ -91,8 +104,8 @@ class TestQuaternionOperations(unittest.TestCase):
             ))
 
     def test_sub(self):
-        a = Quaternion([1,0.2,-3], 0.3)
-        b = Quaternion([2,0.1,-2], 0.8)
+        a = Quaternion([1, 0.2, -3], 0.3)
+        b = Quaternion([2, 0.1, -2], 0.8)
         self.assertTrue(
             within_threshold(
                 a - b,
@@ -106,18 +119,149 @@ class TestQuaternionOperations(unittest.TestCase):
                 Quaternion([-1, 0.1, -1], -0.5)
             ))
 
+    def test_mul(self):
+        a = Quaternion([1, 2, -3], 4)
+        b = Quaternion([2, -1, -4], 1)
+
+        # q = a * b
+        # scalar = a.scalar * b.scalar - dot(a.vector, b.vector)
+        # vector = a.vector * b.scalar + b.vector * a.scalar + cross(a.vector,b.vector)
+
+        truth = Quaternion([-2, -4, -24], -8)
+        self.assertEquals(a * b, truth)
+
+    def test_x(self):
+        q = Quaternion([1, 2, 3], 4)
+        truth = np.mat([
+            [0, -3, 2],
+            [3, 0, -1],
+            [-2, 1, 0],
+        ], dtype=np.float)
+
+        self.assertTrue((q.x==truth).all())
+
 
 class TestQuaternionAngles(unittest.TestCase):
 
     def test_from_rotation(self):
-        q = Quaternion([0,0,1],radians=0)
-        self.assertEquals(Quaternion([0,0,0],1), q)
+        q = Quaternion([0, 0, 1], radians=0)
+        self.assertEquals(Quaternion([0, 0, 0], 1), q)
 
     def test_partial_turn(self):
-        q1 = Quaternion([0,0,1],radians=np.pi/2)
-        q2 = Quaternion([0,0,1],radians=np.pi/2 + np.pi * 2)
+        q1 = Quaternion([0, 0, 1], radians=np.pi/2)
+        q2 = Quaternion([0, 0, 1], radians=np.pi/2 + np.pi * 2)
 
         self.assertTrue(
             within_threshold(q1, -q2)
         )
 
+    def test_is_unit(self):
+
+        units = [
+            ([1, 0, 0], 0),
+            ([0, 0, 0], 1),
+            ([0, -1/np.sqrt(2), 0], -1/np.sqrt(2)),
+        ]
+        for unit in units:
+            q = Quaternion(*unit)
+            self.assertTrue(q.is_unit())
+
+        non_units = [
+            ([2, 0, 0], 0),
+            ([0, 0, 0], 2),
+            ([0, -1/np.sqrt(2), 0.1], -1/np.sqrt(2)),
+        ]
+        for non_unit in non_units:
+            q = Quaternion(*non_unit)
+            self.assertFalse(q.is_unit())
+
+    def test_rotational_matrix(self):
+
+        # Test a 1/4 turn about the z axis
+        q = Quaternion([0, 0, 1], radians=np.pi/2)
+
+        pt = np.mat([1, 0, 1]).T
+        truth = np.mat([0, 1, 1]).T
+
+        new_pt = q.rmatrix * pt
+        self.assertLess(np.sum(np.abs(new_pt - truth)), Quaternion.float_threshold)
+
+        # Test a -1/2 turn about the x axis
+        q = Quaternion([1, 0, 0], radians=-np.pi)
+
+        pt = np.mat([0.2, 0.5, 1]).T
+        truth = np.mat([0.2, -0.5, -1]).T
+
+        new_pt = q.rmatrix * pt
+
+        self.assertLess(np.sum(np.abs(new_pt - truth)), Quaternion.float_threshold)
+
+    def test_rotate_points(self):
+
+        # Rotate a couple points 1/4 turn about the +z axis
+        pts = np.mat([
+            [1, 0, 1],
+            [0.2, 0.5, 1],
+        ])
+
+        q = Quaternion([0, 0, 1], radians=np.pi/2)
+
+        new_pts = q.rotate_points(pts)
+
+        truth = np.mat([
+            [0, 1, 1],
+            [-0.5, 0.2, 1],
+        ])
+
+        self.assertLess(np.sum(np.abs(new_pts - truth)), Quaternion.float_threshold)
+
+
+        pts = np.mat([
+            [1, 0, 0],
+            [0, 1, 0],
+        ])
+        q = Quaternion([1, 0, 0], radians=-np.pi/4)
+
+        new_pts = q.rotate_points(pts)
+
+        truth = np.mat([
+            [1, 0, 0],
+            [0, 1/np.sqrt(2), -1/np.sqrt(2)],
+        ])
+
+        for idx, pt in enumerate(truth):
+            self.assertLess(np.sum(np.abs(new_pts[idx, :] - truth[idx, :])), Quaternion.float_threshold)
+
+    def test_rotate_from_compound_quaternion(self):
+
+        q1 = Quaternion([0, 0, 1], radians=np.pi/2)
+        q2 = Quaternion([1, 0, 0], radians=np.pi/2)
+
+        q = q2 * q1
+
+        pts = np.mat([
+            [1, 0, 0],
+            [1, 0, 1],
+            [0.2, 0.5, 1],
+        ])
+
+        new_pts = q.rotate_points(pts)
+
+        truth = np.mat([
+            [0, 1, 0],
+            [1, 1, 0],
+            [1, 0.2, 0.5],
+        ])
+
+        for idx, pt in enumerate(truth):
+            self.assertLess(np.sum(np.abs(new_pts[idx, :] - truth[idx, :])), Quaternion.float_threshold)
+
+
+class TestIdentityQuaternion(unittest.TestCase):
+
+    def test_identity(self):
+
+        a = Identity()
+        b = Quaternion([0, 0, 0], 1)
+
+        self.assertEquals(a, b)
