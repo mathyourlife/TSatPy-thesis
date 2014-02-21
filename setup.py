381ce210a2b42c94fd50285f148ef4d6882005bd
dcouture <dcouture@dyn.com>
2014-02-21 11:27:41 -0500

    double quoting

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 import TSatPy
 import os
 
-REQ_FILE = os.path.join(os.path.dirname(__file__),"requirements.txt")
+REQ_FILE = os.path.join(os.path.dirname(__file__),'requirements.txt')
 
 # Pull required packages from the requirements.txt file
 with open(REQ_FILE) as rfile:
@@ -23,18 +23,18 @@ classifiers = [
 ]
 
 kwargs = {
-    "name": "tsatpy",
-    "version": TSatPy.__version__,
-    "description": 'TableSat Control Platform',
-    "long_description": open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
-    "author": "Daniel R. Couture",
-    "url": "https://github.com/MathYourLife/TSatPy",
-    "packages": ['TSatPy'],
-    "install_requires": required,
-    "license": 'MIT license',
-    "classifiers": classifiers,
-    "test_suite": 'nose.collector',
-    "tests_require": ['nose', 'coverage'],
+    'name': 'tsatpy',
+    'version': TSatPy.__version__,
+    'description': 'TableSat Control Platform',
+    'long_description': open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+    'author': 'Daniel R. Couture',
+    'url': 'https://github.com/MathYourLife/TSatPy',
+    'packages': ['TSatPy'],
+    'install_requires': required,
+    'license': 'MIT license',
+    'classifiers': classifiers,
+    'test_suite': 'nose.collector',
+    'tests_require': ['nose', 'coverage'],
 }
 
 setup(**kwargs)
