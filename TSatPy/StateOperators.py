ecdae1d0ea876fe66ef692b569a1aa64bdbd0c50
dcouture <dcouture@dyn.com>
2014-02-24 12:24:19 -0500

    State gains

--- TSatPy/StateOperators.py
+++ TSatPy/StateOperators.py
@@ -13,9 +13,13 @@ class BodyRateGain(object):
     Sample::
 
         w = State.BodyRate([1,-1,0])
-        G = StateOperators.BodyRateGain([[1,2,3],[4,5,6],[10,8,9]])
-        print(G * w)
-        # <BodyRate [-1 -1 2]>
+        print('w = %s' % w)
+        Kw = BodyRateGain([[1,2,3],[4,5,6],[10,8,9]])
+        print('Kw = %s' % Kw)
+        print('Kw * w = %s' % (Kw * w))
+        # w = <BodyRate [1 -1 0]>
+        # Kw = [[ 1  2  3]  [ 4  5  6]  [10  8  9]]
+        # Kw * w = <BodyRate [-1 -1 2]>
 
     """
 
@@ -47,7 +51,7 @@ class BodyRateGain(object):
         :return: gain matrix representation
         :rtype: str
         """
-        return str(self.K)
+        return str(self.K).replace('\n', ' ')
 
 
 class QuaternionGain(object):
@@ -58,13 +62,15 @@ class QuaternionGain(object):
     Usage::
 
         q = State.Quaternion([0,0,1], radians=np.pi/10)
-        print 'np.pi/10 = %s' % q
-        qg = QuaternionGain(0.25)
-        print 'qg * q = %s' % (qg * q)
-        print 'np.pi/10 = %s' % State.Quaternion([0,0,1], radians=np.pi/40)
-        # np.pi/10 = <Quaternion [-0 -0 -0.156434], 0.987688>
-        # qg * q = <Quaternion [-0 -0 -0.0392598], 0.999229>
-        # np.pi/40 = <Quaternion [-0 -0 -0.0392598], 0.999229>
+        print('q(pi/10) = %s' % q)
+        Kq = QuaternionGain(0.25)
+        print('Kq = %s' % Kq)
+        print('Kq * q = %s' % (Kq * q))
+        print('q(pi/40) = %s' % State.Quaternion([0,0,1], radians=np.pi/40))
+        # q(pi/10) = <Quaternion [-0 -0 -0.156434], 0.987688>
+        # Kq = 0.25
+        # Kq * q = <Quaternion [-0 -0 -0.0392598], 0.999229>
+        # q(pi/40) = <Quaternion [-0 -0 -0.0392598], 0.999229>
 
     """
     def __init__(self, K):
@@ -101,4 +107,53 @@ class QuaternionGain(object):
         :return: gain matrix representation
         :rtype: str
         """
-        return str(self.K)
+        return str(self.K).replace('\n', ' ')
+
+
+class StateGain(object):
+    """
+    A gain instance for a full state
+
+    :param Kq: Quaternion gain
+    :type  Kq: QuaternionGain
+    :param Kw: Body rate gain
+    :type  Kw: BodyRateGain
+
+    Usage::
+
+        w = State.BodyRate([1,-1,0])
+        q = State.Quaternion([0,0,1], radians=np.pi/10)
+        x = State.State(q, w)
+        print('x = %s' % x)
+        Kq = QuaternionGain(0.25)
+        print('Kq = %s' % Kq)
+        print('Kq * q = %s' % (Kq * q))
+        Kw = BodyRateGain([[1,2,3],[4,5,6],[10,8,9]])
+        print('Kw = %s' % Kw)
+        print('Kw * w = %s' % (Kw * w))
+        Kx = StateGain(Kq, Kw)
+        print('Kx = %s' % Kx)
+        print('Kx * x = %s' % (Kx * x))
+        x = <Quaternion [-0 -0 -0.156434], 0.987688>, <BodyRate [1 -1 0]>
+        Kq = 0.25
+        Kq * q = <Quaternion [-0 -0 -0.0392598], 0.999229>
+        Kw = [[ 1  2  3]  [ 4  5  6]  [10  8  9]]
+        Kw * w = <BodyRate [-1 -1 2]>
+        Kx = <StateGain <Kq 0.25>, <Kw = [[ 1  2  3]  [ 4  5  6]  [10  8  9]]>>
+        Kx * x = <Quaternion [-0 -0 -0.0392598], 0.999229>, <BodyRate [-1 -1 2]>
+
+    """
+
+    def __init__(self, Kq=None, Kw=None):
+        self.Kq = Kq
+        self.Kw = Kw
+
+    def __mul__(self, x):
+        q_new = self.Kq * x.q
+        w_new = self.Kw * x.w
+        return State.State(q_new, w_new)
+
+    def __str__(self):
+        return '<%s <Kq %s>, <Kw = %s>>' % (
+            self.__class__.__name__,
+            str(self.Kq), str(self.Kw))
--- TSatPy/tests/StateOperators_test.py
+++ TSatPy/tests/StateOperators_test.py
@@ -5,14 +5,13 @@ import numpy as np
 
 
 class TestBodyRateGain(unittest.TestCase):
-
     def test_init(self):
         G = StateOperators.BodyRateGain([[1,0],[0,1]])
         self.assertTrue(np.all(G.K == np.mat([[1,0],[0,1]])))
 
     def test_str(self):
         G = StateOperators.BodyRateGain([[1,0],[0,1]])
-        g_str = '[[1 0]\n [0 1]]'
+        g_str = '[[1 0]  [0 1]]'
         self.assertEquals(g_str, str(G))
 
     def test_mul(self):
@@ -24,7 +23,6 @@ class TestBodyRateGain(unittest.TestCase):
 
 
 class TestQuaternionGain(unittest.TestCase):
-
     def test_init(self):
         G = StateOperators.QuaternionGain(0.5)
         self.assertEquals(G.K, 0.5)
@@ -40,3 +38,33 @@ class TestQuaternionGain(unittest.TestCase):
         q_new = (qg * q)
         q_expected = State.Quaternion([0,0,1], radians=np.pi/40)
         self.assertEquals(q_new, q_expected)
+
+
+class TestStateGain(unittest.TestCase):
+    def test_init(self):
+        Kq = StateOperators.QuaternionGain(0.5)
+        Kw = StateOperators.BodyRateGain([[1,0],[0,1]])
+        Kx = StateOperators.StateGain(Kq, Kw)
+        self.assertEquals(Kx.Kq, Kq)
+        self.assertEquals(Kx.Kw, Kw)
+
+    def test_str(self):
+        Kq = StateOperators.QuaternionGain(0.5)
+        Kw = StateOperators.BodyRateGain([[1,0],[0,1]])
+        Kx = StateOperators.StateGain(Kq, Kw)
+        g_str = '<StateGain <Kq 0.5>, <Kw = [[1 0]  [0 1]]>>'
+        self.assertEquals(g_str, str(Kx))
+
+    def test_gain(self):
+        w = State.BodyRate([1,-1,0])
+        q = State.Quaternion([0,0,1], radians=np.pi/10)
+        x = State.State(q, w)
+        Kq = StateOperators.QuaternionGain(0.25)
+        Kw = StateOperators.BodyRateGain([[1,2,3],[4,5,6],[10,8,9]])
+        Kx = StateOperators.StateGain(Kq, Kw)
+
+        w_expected = State.BodyRate([-1,-1,2])
+        q_expected = State.Quaternion([0,0,1], radians=np.pi/40)
+        x_expected = State.State(q_expected, w_expected)
+
+        self.assertEquals(x_expected, Kx * x)
