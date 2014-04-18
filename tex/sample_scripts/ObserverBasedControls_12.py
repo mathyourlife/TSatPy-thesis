a54b0d507b102c6f18e72a580c74774e94379164
dcouture <dcouture@dyn.com>
2014-04-18 11:45:54 -0400

    Adding sample scripts appendix

---
+++ tex/sample_scripts/ObserverBasedControls_12.py
@@ -0,0 +1,8 @@
+from TSatPy import Estimator, State
+from TSatPy.Clock import Metronome
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import random
+
+print('Quaternion Decomposition')
